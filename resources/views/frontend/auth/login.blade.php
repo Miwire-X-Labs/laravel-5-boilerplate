711ff62621cf4086f8887519d792667e194362a1
Anthony Rappa <rappa819@gmail.com>
2016-04-22 20:35:07 -0400

    - Corrected blade syntax in change, email, reset, login, register.blade.php - Added guest() method to access class - Corrected blade syntax in frontend nav and converted html links to link helper methods. - Corrected blade syntax in profile/edit.blade.php, user/dashboard.blade.php, macros.blade.php, ga.blade.php, lang.blade.php - Converted html links to link helper methods in user/dashboard.blade.php - Corrected blade syntax in messages.blade.php and used session() helper instead of facade.

--- resources/views/frontend/auth/login.blade.php
+++ resources/views/frontend/auth/login.blade.php
@@ -11,19 +11,19 @@
 
                 <div class="panel-body">
 
-                    {!! Form::open(['url' => 'login', 'class' => 'form-horizontal']) !!}
+                    {{ Form::open(['url' => 'login', 'class' => 'form-horizontal']) }}
 
                         <div class="form-group">
-                            {!! Form::label('email', trans('validation.attributes.frontend.email'), ['class' => 'col-md-4 control-label']) !!}
+                            {{ Form::label('email', trans('validation.attributes.frontend.email'), ['class' => 'col-md-4 control-label']) }}
                             <div class="col-md-6">
-                                {!! Form::input('email', 'email', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.email')]) !!}
+                                {{ Form::input('email', 'email', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.email')]) }}
                             </div><!--col-md-6-->
                         </div><!--form-group-->
 
                         <div class="form-group">
-                            {!! Form::label('password', trans('validation.attributes.frontend.password'), ['class' => 'col-md-4 control-label']) !!}
+                            {{ Form::label('password', trans('validation.attributes.frontend.password'), ['class' => 'col-md-4 control-label']) }}
                             <div class="col-md-6">
-                                {!! Form::input('password', 'password', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.password')]) !!}
+                                {{ Form::input('password', 'password', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.password')]) }}
                             </div><!--col-md-6-->
                         </div><!--form-group-->
 
@@ -31,7 +31,7 @@
                             <div class="col-md-6 col-md-offset-4">
                                 <div class="checkbox">
                                     <label>
-                                        {!! Form::checkbox('remember') !!} {{ trans('labels.frontend.auth.remember_me') }}
+                                        {{ Form::checkbox('remember') }} {{ trans('labels.frontend.auth.remember_me') }}
                                     </label>
                                 </div>
                             </div><!--col-md-6-->
@@ -39,13 +39,13 @@
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
-                                {!! Form::submit(trans('labels.frontend.auth.login_button'), ['class' => 'btn btn-primary', 'style' => 'margin-right:15px']) !!}
+                                {{ Form::submit(trans('labels.frontend.auth.login_button'), ['class' => 'btn btn-primary', 'style' => 'margin-right:15px']) }}
 
-                                {!! link_to('password/reset', trans('labels.frontend.passwords.forgot_password')) !!}
+                                {{ link_to('password/reset', trans('labels.frontend.passwords.forgot_password')) }}
                             </div><!--col-md-6-->
                         </div><!--form-group-->
 
-                    {!! Form::close() !!}
+                    {{ Form::close() }}
 
                     <div class="row text-center">
                         {!! $socialite_links !!}
