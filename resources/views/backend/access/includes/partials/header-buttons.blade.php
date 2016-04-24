baf6971f9c7529aab95420f004591a898ff2e681
Anthony Rappa <rappa819@gmail.com>
2016-04-24 01:32:29 -0400

    Added margin padding helpers.

--- resources/views/backend/access/includes/partials/header-buttons.blade.php
+++ resources/views/backend/access/includes/partials/header-buttons.blade.php
@@ -1,9 +1,10 @@
-    <div class="pull-right" style="margin-bottom:10px">
-        <div class="btn-group">
-          <button type="button" class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-              {{ trans('menus.backend.access.users.main') }} <span class="caret"></span>
-          </button>
-          <ul class="dropdown-menu" role="menu">
+<div class="pull-right mb-10">
+    <div class="btn-group">
+        <button type="button" class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+          {{ trans('menus.backend.access.users.main') }} <span class="caret"></span>
+        </button>
+
+        <ul class="dropdown-menu" role="menu">
             <li><a href="{{ route('admin.access.users.index') }}">{{ trans('menus.backend.access.users.all') }}</a></li>
 
             @permission('create-users')
@@ -13,28 +14,29 @@
             <li class="divider"></li>
             <li><a href="{{ route('admin.access.users.deactivated') }}">{{ trans('menus.backend.access.users.deactivated') }}</a></li>
             <li><a href="{{ route('admin.access.users.deleted') }}">{{ trans('menus.backend.access.users.deleted') }}</a></li>
-          </ul>
-        </div><!--btn group-->
+        </ul>
+    </div><!--btn group-->
 
-        <div class="btn-group">
-          <button type="button" class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-              {{ trans('menus.backend.access.roles.main') }} <span class="caret"></span>
-          </button>
-          <ul class="dropdown-menu" role="menu">
+    <div class="btn-group">
+        <button type="button" class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+          {{ trans('menus.backend.access.roles.main') }} <span class="caret"></span>
+        </button>
+
+        <ul class="dropdown-menu" role="menu">
             <li><a href="{{ route('admin.access.roles.index') }}">{{ trans('menus.backend.access.roles.all') }}</a></li>
 
             @permission('create-roles')
                 <li><a href="{{ route('admin.access.roles.create') }}">{{ trans('menus.backend.access.roles.create') }}</a></li>
             @endauth
-          </ul>
-        </div><!--btn group-->
+        </ul>
+    </div><!--btn group-->
 
-        <div class="btn-group">
-          <button type="button" class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-              {{ trans('menus.backend.access.permissions.main') }} <span class="caret"></span>
-          </button>
-          <ul class="dropdown-menu pull-right" role="menu">
+    <div class="btn-group">
+        <button type="button" class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+          {{ trans('menus.backend.access.permissions.main') }} <span class="caret"></span>
+        </button>
 
+        <ul class="dropdown-menu pull-right" role="menu">
             @permission('create-permission-groups')
                 <li><a href="{{ route('admin.access.roles.permission-group.create') }}">{{ trans('menus.backend.access.permissions.groups.create') }}</a></li>
             @endauth
@@ -49,8 +51,8 @@
 
             <li><a href="{{ route('admin.access.roles.permissions.index') }}#all-permissions">{{ trans('menus.backend.access.permissions.all') }}</a></li>
             <li><a href="{{ route('admin.access.roles.permissions.index') }}">{{ trans('menus.backend.access.permissions.groups.all') }}</a></li>
-          </ul>
-        </div><!--btn group-->
-    </div><!--pull right-->
+        </ul>
+    </div><!--btn group-->
+</div><!--pull right-->
 
-    <div class="clearfix"></div>
+<div class="clearfix"></div>
